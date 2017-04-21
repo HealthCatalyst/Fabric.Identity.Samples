99f87e1f4b8c9c92a58573a0af0784599d5605b9
Kyle Paul <djjoebanks@gmail.com>
2017-04-21 16:08:38 -0700

    updated post logout redirect url to match what is registered with the identity server

--- Angular/src/app/shared/services/auth.service.ts
+++ Angular/src/app/shared/services/auth.service.ts
@@ -6,7 +6,7 @@ const clientSettings: any = {
   authority: 'http://localhost:5001/',
   client_id: 'fabric-angularsample',
   redirect_uri: 'http://localhost:4200/oidc-callback.html',
-  post_logout_redirect_uri: 'http://localhost:4200/',
+  post_logout_redirect_uri: 'http://localhost:4200',
   response_type: 'id_token token',
   scope: 'openid profile fabric.profile patientapi',
 
