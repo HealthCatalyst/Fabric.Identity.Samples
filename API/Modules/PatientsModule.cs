d303d6989c7bfc7b7fc193efd816543c65a07b16
Kyle Paul <djjoebanks@gmail.com>
2017-09-05 11:41:03 -0700

    missed file in last commit. add mrn to sample patient data

--- API/Modules/PatientsModule.cs
+++ API/Modules/PatientsModule.cs
@@ -18,18 +18,21 @@ namespace Fabric.Identity.Samples.API.Modules
             {
                 new
                 {
+                    Id = 1,
                     FirstName = "Matt",
                     LastName = "Murdock",
                     DateOfBirth = DateTime.Parse("03/27/1965")
                 },
                 new
                 {
+                    Id = 2,
                     FirstName = "Jessica",
                     LastName = "Jones",
                     DateOfBirth = DateTime.Parse("06/12/1972")
                 },
                 new
                 {
+                    Id = 3,
                     FirstName = "Luke",
                     LastName = "Cage",
                     DateOfBirth = DateTime.Parse("05/29/1980")
