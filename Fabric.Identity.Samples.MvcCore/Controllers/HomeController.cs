44330b82042c3acf24153260313f73f06637ca2b
Brian Smith <brian.smith@healthcatalyst.com>
2018-06-04 14:54:39 -0400

    1) removed sample code related to Fabric.Authorization and other external services, 2) print out claims on MvcCore sample, 3) minor cleanup

--- Fabric.Identity.Samples.MvcCore/Controllers/HomeController.cs
+++ Fabric.Identity.Samples.MvcCore/Controllers/HomeController.cs
@@ -1,56 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using Fabric.Identity.Samples.MvcCore.Configuration;
-using Fabric.Identity.Samples.MvcCore.Services;
-using Fabric.Platform.Http;
-using Microsoft.AspNetCore.Authentication;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 
 namespace Fabric.Identity.Samples.MvcCore.Controllers
 {
-
     [Authorize]
     public class HomeController : Controller
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly IFabricAuthorizationService _fabricAuthorizationService;
-        private readonly IAppConfiguration _appConfiguration;
-
-        public HomeController(IHttpClientFactory httpClientFactory, IFabricAuthorizationService authorizationService, IAppConfiguration appConfiguration)
-        {
-            _httpClientFactory = httpClientFactory;
-            _fabricAuthorizationService = authorizationService ??
-                                    throw new ArgumentNullException(nameof(authorizationService));
-
-            _appConfiguration = appConfiguration ??
-                                          throw new ArgumentNullException(nameof(appConfiguration));
-        }
         public IActionResult Index()
         {
             return View();
         }
-        public async Task<IActionResult> Patient()
-        {
-            var clientId = _appConfiguration.IdentityServerConfidentialClientSettings.ClientId;
 
-            var permissions = GetPermissionsForUserInternal().Result;
-            ViewBag.UserPermissions = permissions;
-            if (permissions.Permissions.Contains($"app/${clientId}.viewpatient"))
-            {
-                var accessToken = await HttpContext.Authentication.GetTokenAsync("access_token");
-                ViewBag.HasEditPatientPermission = permissions.Permissions.Contains($"app/{clientId}.editpatient");
-                return await CallApiWithToken(accessToken);
-            }
-            ViewBag.ErrorMessage = "This user does not have permission to view patients";
-            return View("Patient");
-        }
-        
         public IActionResult Error()
         {
             return View();
@@ -61,84 +22,6 @@ namespace Fabric.Identity.Samples.MvcCore.Controllers
             await HttpContext.Authentication.SignOutAsync("Cookies");
             await HttpContext.Authentication.SignOutAsync("oidc");
         }
-
-        public async Task<IActionResult> SetupRolesAndPermissions()
-        {
-            var clientId = _appConfiguration.IdentityServerConfidentialClientSettings.ClientId;
-            var viewerGroup = await SetupGroup(
-                new {Grain = "app", SecurableItem = clientId, Name = "viewpatient"},
-                new {Grain = "app", SecurableItem = clientId, Name = "viewer"}, @"FABRIC\Health Catalyst Viewer").Result;
-
-            var editorGroup = await SetupGroup(
-                new {Grain = "app", SecurableItem = clientId, Name = "editpatient"},
-                new {Grain = "app", SecurableItem = clientId, Name = "editor"}, @"FABRIC\Health Catalyst Editor").Result;
-
-            ViewBag.CreatedGroups = new List<dynamic> { viewerGroup, editorGroup };
-            return View("Json");
-        }
-
-        public async Task<IActionResult> GetPermissionsForUser()
-        {
-            var permissions = await GetPermissionsForUserInternal();
-            ViewBag.UserPermissions = permissions;
-            return View("Json");
-        }
-
-        public async Task<dynamic> GetGroupsRolesAndPermissions()
-        {
-            var accessToken = await HttpContext.Authentication.GetTokenAsync("access_token");
-            _fabricAuthorizationService.SetAccessToken(accessToken);
-            var viewerGroup = await _fabricAuthorizationService.GetGroupByName(@"FABRIC\Health Catalyst Viewer");
-            var editorGroup = await _fabricAuthorizationService.GetGroupByName(@"FABRIC\Health Catalyst Editor");
-
-            ViewBag.CreatedGroups = new List<dynamic> { viewerGroup, editorGroup };
-            return View("Json");
-        }
-
-        private async Task<UserPermissions> GetPermissionsForUserInternal()
-        {
-            var accessToken = await HttpContext.Authentication.GetTokenAsync("access_token");
-            _fabricAuthorizationService.SetAccessToken(accessToken);
-            return _fabricAuthorizationService.GetPermissionsForUser("app", _appConfiguration.IdentityServerConfidentialClientSettings.ClientId).Result;
-        }
-
-        private async Task<dynamic> SetupGroup(dynamic permission, dynamic role, string group)
-        {
-            var accessToken = await HttpContext.Authentication.GetTokenAsync("access_token");
-            _fabricAuthorizationService.SetAccessToken(accessToken);
-            permission = await _fabricAuthorizationService.CreatePermission(permission);
-            role = await _fabricAuthorizationService.CreatRole(role);
-            await _fabricAuthorizationService.AddPermissionToRole(permission, role);
-            await _fabricAuthorizationService.AddRoleToGroup(role, group);
-            return _fabricAuthorizationService.GetGroupByName(group);
-        }
-
-        private async Task<IActionResult> CallApiWithToken(string accessToken)
-        {
-            var uri = new Uri("http://localhost:5003/patients/123");
-            var client = _httpClientFactory.CreateWithAccessToken(uri, accessToken);
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            var response = await client.GetAsync(uri);
-            if (response.IsSuccessStatusCode)
-            {
-                ViewBag.PatientDataResponse = JsonConvert.DeserializeObject<PatientDataResponse>(await response.Content.ReadAsStringAsync());
-                return View("Patient");
-            }
-
-            if (response.StatusCode == HttpStatusCode.Forbidden)
-            {
-                ViewBag.ErrorMessage = $"Received 403 Forbidden when calling: {uri}";
-                return View("Patient");
-            }
-            throw new Exception($"Error received: {response.StatusCode} when trying to contact remote server: {uri}");
-        }
-    }
-
-    public class PatientDataResponse
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime DateOfBirth { get; set; }
     }
 
     public class UserClaim
@@ -146,6 +29,4 @@ namespace Fabric.Identity.Samples.MvcCore.Controllers
         public string Type { get; set; }
         public string Value { get; set; }
     }
-
-    
 }
--- Fabric.Identity.Samples.MvcCore/README.md
+++ Fabric.Identity.Samples.MvcCore/README.md
@@ -1,14 +1,7 @@
-﻿# Fabric Identity & Authorization Setup
-To run the ASP.NET Core MVC sample, you will need to set up a client in your local instances of Fabric.Identity and Fabric.Authorization.
+﻿# Fabric Identity Setup
+To run the ASP.NET Core MVC sample, you will need to set up a client in your local instances of Fabric.Identity.
 
 ## Fabric.Identity Client
-
-### Prerequisites
-* This document assumes you have Fabric.Identity and Fabric.Authorization installed locally.
-
-* To run the sample as is, you will need an `ApiResource` registered with Fabric.Identity that has an `ApiScope` of `patientapi`. If this does not exist, then you will need to remove the `patientapi` scope on line 96 of `Startup.cs`.
-
-### Setup
 Assuming your Fabric.Identity instance is running at `http://localhost/identity`, you can run the following `curl` command to create the Fabric.Identity.Samples.CoreMvc client:
 
 `curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer {access_token}" -d @{identity_client.json} http://localhost/identity/api/client`
@@ -25,7 +18,6 @@ and `{identity_client.json}` is a file containing the following payload:
         "openid",
         "profile",
         "fabric.profile",
-        "patientapi",
         "offline_access",
         "fabric/authorization.read",
         "fabric/authorization.write"
@@ -47,24 +39,6 @@ and `{identity_client.json}` is a file containing the following payload:
 _Note the `redirectUris` and `postLogoutRedirectUris` assume the sample app is running at http://localhost:5002._
 
 The `IdentityServerConfidentialClientSettings.ClientSecret` in `appSettings.json` should be set to the `clientSecret` contained in the JSON response of the `curl` command.
-
-## Fabric.Authorization Client
-
-Assuming your Fabric.Authorization instance is running at `http://localhost/authorization`, you can run the following `curl` command to create a Fabric.Authorization client:
-
-`curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer {access_token}" -d @{auth_client.json} http://localhost/authorization/v1/clients`
-
-where `{access_token}` is the JWT obtained per the instructions at [Retrieving an Access Token from Fabric.Identity](https://github.com/HealthCatalyst/Fabric.Identity/wiki/Retrieving-an-Access-Token-from-Fabric.Identity)
-
-and `{auth_client.json}` is a file containing the following payload:
-
-```
-{
-	"id": "fabric-mvc-core-sample",
-	"name": "Fabric .NET MVC Core Sample",
-	"topLevelSecurableItem": {"name": "fabric-mvc-core-sample"}	
-}
-```
 
 ## Running the Sample Application
 Once you complete the steps above, you can execute the sample by running through Visual Studio debugger and navigating to `http://localhost:5002` or installing the application on your local IIS.
