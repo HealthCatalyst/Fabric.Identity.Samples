328c19ba9bf79ad6443d44ef5b359fc48ae249d1
Brian Smith <brian.smith@healthcatalyst.com>
2018-06-12 16:33:39 -0400

    1) fixed MVC full framework hybrid sample so that client secret validation occurs, 2) refactored MVC Core hybrid sample so that its functionality matches the full framework sample

--- Fabric.Identity.Samples.MvcCore/Controllers/HomeController.cs
+++ Fabric.Identity.Samples.MvcCore/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fabric.Identity.Samples.MvcCore.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
@@ -12,8 +14,30 @@ namespace Fabric.Identity.Samples.MvcCore.Controllers
             return View();
         }
 
-        public IActionResult Error()
+        [Authorize]
+        public async Task<IActionResult> Claims()
         {
+            ViewBag.Message = "Claims";
+
+            var cp = (ClaimsPrincipal)User;
+            await AddTokensToClaims(cp);
+            ViewData["access_token"] = cp.FindFirst("access_token").Value;
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> MyIdentityClient()
+        {
+            var cp = (ClaimsPrincipal)User;
+            await AddTokensToClaims(cp);
+
+            var token = cp.FindFirst("access_token").Value;
+
+            var client = new HttpClient();
+            client.SetBearerToken(token);
+
+            var result = await client.GetStringAsync("{identity-url}/api/client/fabric-mvc-sample");
+            ViewBag.Json = result;
             return View();
         }
 
@@ -22,11 +46,20 @@ namespace Fabric.Identity.Samples.MvcCore.Controllers
             await HttpContext.Authentication.SignOutAsync("Cookies");
             await HttpContext.Authentication.SignOutAsync("oidc");
         }
-    }
 
-    public class UserClaim
-    {
-        public string Type { get; set; }
-        public string Value { get; set; }
+        private async Task AddTokensToClaims(ClaimsPrincipal user)
+        {
+            var oidcInfo = await Request.HttpContext.Authentication.GetAuthenticateInfoAsync("oidc");
+            var accessToken = oidcInfo.Properties.Items[".Token.access_token"];
+            var refreshToken = oidcInfo.Properties.Items[".Token.refresh_token"];
+            var idToken = oidcInfo.Properties.Items[".Token.id_token"];
+            var expiresAt = oidcInfo.Properties.Items[".Token.expires_at"];
+            var oidcIdentity = user.Identities.First(i => i.AuthenticationType == "AuthenticationTypes.Federation");
+
+            oidcIdentity.AddClaim(new Claim("access_token", accessToken));
+            oidcIdentity.AddClaim(new Claim("refresh_token", refreshToken));
+            oidcIdentity.AddClaim(new Claim("id_token", idToken));
+            oidcIdentity.AddClaim(new Claim("expires_at", expiresAt));
+        }
     }
 }
